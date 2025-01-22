5a3600389341862ee9a30c4d5aa02afd76db2c80
SunmeetSunns <98201485+SunmeetSunns@users.noreply.github.com>
2025-01-22 20:27:07 +0530

    Revert "build updated successfully and angular.json modified"
    This reverts commit 43e786459356145c03ce725b9ebf065dfe382850.

---
+++ wealthSphere-frontend/dist/wealth-sphere-frontend/browser/chunk-UTCNCN5Q.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-VBDOPD3S.js";import"./chunk-LNW6KWMN.js";export{a as AddInvestmentsModule};
---
+++ wealthSphere-frontend/dist/wealth-sphere-frontend/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-NTFYGVBN.mjs";import"./chunk-PLAEW5IH.mjs";import"./chunk-S6PDCKWI.mjs";import"./chunk-WGGTSU2I.mjs";import"./chunk-I3C2H4K4.mjs";import"./chunk-QIXVHZRN.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};

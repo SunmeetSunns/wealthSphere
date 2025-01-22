f3407bdb8bccc5d58b36fb11b5273433f417fb3b
Sunmeet Sunns <kaursunmeet624@gmail.com>
2025-01-22 14:25:04 +0530

    database file updated

--- wealthSphere-backend/db.js
+++ wealthSphere-backend/db.js
@@ -5,11 +5,12 @@ let db;
 
 const connectDB = async () => {
   try {
-    const client = await mongoose.connect(process.env.MONGO_URL);
+    const client = await mongoose.connect(process.env.MONGO_URL); // No options needed here anymore
     db = client.connection.db; // Get the database instance
     console.log('MongoDB connected');
   } catch (err) {
-    console.error('MongoDB connection error:', err);
+    console.error('MongoDB connection error:', err.message || err);
+    process.exit(1); // Exit if connection fails
   }
 };
 

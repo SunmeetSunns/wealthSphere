89cb465ee965e31a1c0964cc7430af193a74904a
SunmeetSunns <98201485+SunmeetSunns@users.noreply.github.com>
2025-01-23 22:24:59 +0530

    cors updated

--- wealthSphere-backend/server.js
+++ wealthSphere-backend/server.js
@@ -2,31 +2,33 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const portfolioRoutes = require('./router/portfolioRoutes');
-const userRoute=require('./router/user')
+const userRoute = require('./router/user');
 const Counter = require('./models/counter'); // Import the Counter model
 const { connectDB } = require('./db'); // Import the connectDB function
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
-const {verifyUser}=require('./middleware/auth')
+const { verifyUser } = require('./middleware/auth');
 
 const app = express();
 
-// Middleware
+const allowedOrigins = ['https://deft-starship-250f12.netlify.app', 'http://localhost:4200'];
+
 app.use(cors({
-  origin:'https://deft-starship-250f12.netlify.app/',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  origin: '*',
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   credentials:true
-}
- 
-));
+}));
+
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// Environment variables
 const port = process.env.PORT || 5000;
 
-// Initialize routes
-app.use('/api/user',userRoute);
-app.use('/api/portfolio',verifyUser, portfolioRoutes);
+// Routes
+app.use('/api/user', userRoute);
+app.use('/api/portfolio', verifyUser, portfolioRoutes);
 
 // Initialize the counter for orderId
 const initCounter = async () => {
@@ -42,11 +44,18 @@ const initCounter = async () => {
 };
 
 // MongoDB connection and counter initialization
-connectDB()
-  .then(initCounter) // Initialize counter after successful connection
-  .catch(err => console.error('Error during initialization:', err));
+(async () => {
+  try {
+    await connectDB(); // Connect to MongoDB
+    await initCounter(); // Initialize the counter
+    console.log('Database connected and counter initialized.');
+  } catch (err) {
+    console.error('Error during initialization:', err);
+    process.exit(1); // Exit if there's an error
+  }
+})();
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is listening at port ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
